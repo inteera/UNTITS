0d03c11c1a622a434354b940d58b4722c85192a9
Arda Coşkun <inteera@protonmail.com>
2023-02-25 18:15:21 +0300

    Update video_player_page.dart

--- lib/video_player_page.dart
+++ lib/video_player_page.dart
@@ -68,12 +68,6 @@ class _VideoAppState extends State<VideoApp> {
   }
   _saveVideo() async {
 
-     var appDocDir = await getTemporaryDirectory();
-    String savePath = appDocDir.path + "/temp.mp4";
-    await Dio().download("https://instagram.fist1-2.fna.fbcdn.net/o1/v/t16/f1/m78/5046581EA39BC2BEF7FF05A4ACC4298B_video_dashinit.mp4?efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uNzIwLnN0b3J5LmJhc2VsaW5lIn0&_nc_ht=instagram.fist1-2.fna.fbcdn.net&_nc_cat=104&vs=705169128056251_2998598360&_nc_vs=HBksFQIYUWlnX3hwdl9wbGFjZW1lbnRfcGVybWFuZW50X3YyLzUwNDY1ODFFQTM5QkMyQkVGN0ZGMDVBNEFDQzQyOThCX3ZpZGVvX2Rhc2hpbml0Lm1wNBUAAsgBABUAGCRHSzJBMmhOLWhNM1U4M3dGQU0wbDJiZ2ZybkZFYnBrd0FBQUYVAgLIAQAoABgAGwGIB3VzZV9vaWwBMRUAACbE5ffir63NQBUCKAJDMywXQDDu2RaHKwIYEmRhc2hfYmFzZWxpbmVfMV92MREAdegHAA%3D%3D&_nc_rid=0e2512e340&ccb=9-4&oh=00_AfAHpsSSi8BTpBRRi2k5k8fypbTf_SkNmRrByuTwbNrBTw&oe=63FA73F5&_nc_sid=21929d", savePath);
-    final result = await ImageGallerySaver.saveFile(savePath);
-    await Dio().delete(savePath);
-    print(result);
 
 
   }
